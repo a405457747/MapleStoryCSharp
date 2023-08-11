980752048c595dc2f7f3457c1bc39e7e56715bd3
Sky.Allen <894982165@qq.com>
2023-08-11 16:21:07 +0800

    1

--- Assets/MapleStory/Samples/MenuManager/NumberRollSample.cs
+++ Assets/MapleStory/Samples/MenuManager/NumberRollSample.cs
@@ -12,27 +12,31 @@ using DG.Tweening;
 
 public class NumberRollSample : MonoBehaviour
 {
-
-
     public Text text;
     private Sequence mScoreSequence;
     private int mOldScore = 0;
     private int newScore = 0;
+
     void Awake()
     {
         mScoreSequence = DOTween.Sequence();
         mScoreSequence.SetAutoKill(false);
     }
+
     void Start()
     {
     }
-    void DigitalAnimation() {
-        mScoreSequence.Append(DOTween.To(delegate (float value) {
+
+    void DigitalAnimation()
+    {
+        mScoreSequence.Append(DOTween.To(delegate(float value)
+        {
             var temp = Math.Floor(value);
             text.text = temp + "";
         }, mOldScore, newScore, 5f));
         mOldScore = newScore;
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
@@ -41,8 +45,4 @@ public class NumberRollSample : MonoBehaviour
             DigitalAnimation();
         }
     }
-
-
-
-
 }
