4f59894d70c43237b0d0ceb4519917758e52dfb9
Sky.Allen <894982165@qq.com>
2023-10-13 17:33:15 +0800

    1

--- MapleStory2/MapleStory2/Assets/GameApp/Scripts/ToolRoot.cs
+++ MapleStory2/MapleStory2/Assets/GameApp/Scripts/ToolRoot.cs
@@ -7,6 +7,30 @@ namespace MapleStory2
     //代替原先的helper
     public  class ToolRoot 
     {
-        
+        public static T FindComponent<T>(MonoBehaviour mono, string targetName) where T : Component
+        {
+
+            Transform FindTrans(Transform aParent, string aName)
+            {
+                foreach (Transform child in aParent)
+                {
+                    if (child.name == aName) return child;
+
+                    if (child.name.Contains("GameObject") && (child.name.StartsWith("GameObject") == false))
+                    {
+                    }
+                    else
+                    {
+                        var result = FindTrans(child, aName);
+                        if (result != null) return result;
+                    }
+                }
+
+                return null;
+            }
+
+            var t = FindTrans(mono.transform, targetName);
+            return t != null ? t.GetComponent<T>() : default;
+        }
     }
 }
