0e38988bfa84166ae1f3b0f201c9515d930b6e9c
Sky.Allen <894982165@qq.com>
2023-10-14 16:31:07 +0800

    1

---
+++ MapleStory2/MapleStory2/Assets/GameApp/Scripts/Config/TestData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson.Extensions;
+
+
+namespace MapleStory2
+{
+    [CreateAssetMenu(fileName = "TestData", menuName = "ColaFramework/TestData")]
+    public class TestData : ScriptableObject
+    {
+        public bool aa;
+        public bool bb;
+        public Vector3 kkkl;
+    }
+}
